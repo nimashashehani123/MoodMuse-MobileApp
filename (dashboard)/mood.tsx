346a76a5d4820419d900acc9864e9e9bbb89836e
T.L. Nimasha Shehani <nimashashehani0715@gmail.com>
2025-09-02 14:37:58 +0530

    add main tabs (home,journal,mood,profile)

---
+++ (dashboard)/mood.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const mood = () => {
+  return (
+    <View>
+      <Text>mood</Text>
+    </View>
+  )
+}
+
+export default mood
+
+const styles = StyleSheet.create({})
